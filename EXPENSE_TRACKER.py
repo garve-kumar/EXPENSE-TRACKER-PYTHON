c88f2927023d2a2ce0b80da2a8b44e267290be88
garve-kumar <garvkumar062005@gmail.com>
2026-05-28 17:12:28 +0530

    First expense tracker project

---
+++ EXPENSE_TRACKER.py
@@ -0,0 +1,51 @@
+expenses = []
+
+print("1. Add Expense")
+print("2. view Expense")
+print("3. Show Expense") 
+print("4, delete expense")
+print("5. Exit\n")
+while True:
+
+    a =  int(input("Enter your choice: "))
+    if a == 1:
+       print("You selected Add Expense")
+       name = input("Enter expense name: ")
+       amount = int(input("Enter expense amount: "))
+       expense = {"name": name, "amount": amount}
+       expenses.append(expense)
+       file = open("expenses.txt", "a")
+       file.write(name + " - " + str(amount) + "\n")
+       file.close()
+       print("Expense added successfully")
+    if a == 2:
+       print("you selected view expense")
+       file = open("expenses.txt", "r")
+       data = file.read()
+       print(data)
+       file.close
+       for expense in expenses:
+          print("Expense Name: ", expense["name"])
+          print("Expense Amount", expense['amount'])
+    if a == 3:
+         total = 0
+         for expense in expenses:
+               total = total + expense["amount"]
+         print("Total  expense: ", total)
+    if a == 4:
+        if len(expense) == 0:
+            print("NO expense to delete")
+        else:
+                 for index, expense in enumerate(expenses):
+                  print(index + 1, expense["name"], "-",expense["amount"])
+                 delete_index = int(input("Enter the expense number to delete: "))
+                 expenses.pop(delete_index - 1)
+                 print("Expense Deleted Successfully")
+                
+                
+    if a == 5:
+        print("Exiting Program....")
+        break
+            
+             
+                 
